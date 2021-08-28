92b442543fb0539f7e475b9c1736e5903e004881
Andy Jenkins <andy@4gclinical.com>
2021-08-28 11:37:44 -0700

    works, navigate to localhost

--- pacifica_visualizer/views.py
+++ pacifica_visualizer/views.py
@@ -3,12 +3,14 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 
 from pacifica_visualizer.models import Sheet
+from pacifica_visualizer.sheets import get_sheet_detail
 
 ...
 
 
 @api_view(['GET', 'POST', 'DELETE'])
 def sheet_list(request):
+    return JsonResponse(get_sheet_detail(), status=status.HTTP_200_OK)
     print("returning sheet list")
 
 # GET list of tutorials, POST a new tutorial, DELETE all tutorials
@@ -18,7 +20,8 @@ def sheet_list(request):
 def sheet_detail(request, pk):
     # find tutorial by pk (id)
     try:
-        tutorial = Sheet.objects.get(pk=pk)
+        # tutorial = Sheet.objects.get(pk=pk)
+        return JsonResponse(get_sheet_detail(), status=status.HTTP_200_OK)
     except Sheet.DoesNotExist:
         return JsonResponse({'message': 'The tutorial does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
@@ -27,5 +30,5 @@ def sheet_detail(request, pk):
 
 @api_view(['GET'])
 def sheet_list_active(request):
-# GET all published tutorials
+    return JsonResponse(get_sheet_detail(), status=status.HTTP_200_OK)
     print("returning sheet list active")
--- pacificafrontend/src/app/services/sheet.service.ts
+++ pacificafrontend/src/app/services/sheet.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import { Sheet } from '../models/sheet.model';
+
+// @ts-ignore
 @Injectable({
   providedIn: 'root'
 })
