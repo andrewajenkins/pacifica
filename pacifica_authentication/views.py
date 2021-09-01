e3a64b2116bddda7a11f637fa34c4c23ccbf33bb
Andy Jenkins <andy@4gclinical.com>
2021-08-31 18:57:47 -0700

    add lockout after too many failed login attempts (untested)

--- pacifica_authentication/views.py
+++ pacifica_authentication/views.py
@@ -1,10 +1,18 @@
+import logging
+from arrow import arrow
 from django.contrib.auth import get_user_model
+from django.core.cache import cache
+from requests import Response
 
 from rest_framework.generics import RetrieveAPIView
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import serializers
+from rest_framework import serializers, status
+from django.utils.translation import gettext_lazy as _
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken, AuthenticationFailed
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+logger = logging.getLogger(__name__)
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -19,4 +27,76 @@ class UserAPIView(RetrieveAPIView):
     serializer_class = UserSerializer
 
     def get_object(self):
-        return self.request.user
+        print("get_object")
+        from pprint import pprint
+        pprint(vars(self.request))
+        pprint(vars(self.request.user))
+
+        return None
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        from pprint import pprint
+        pprint(request.data)
+        pprint(vars(request))
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
+
+        locked_out = False
+        email = request.data.username
+        cache_results = InvalidLoginAttemptsCache.get(email)
+        if cache_results and cache_results.get('lockout_start'):
+            lockout_start = arrow.get(cache_results.get('lockout_start'))
+            locked_out = lockout_start >= arrow.utcnow().shift(minutes=-15)
+            if not locked_out:
+                InvalidLoginAttemptsCache.delete(email)
+            else:
+                raise AuthenticationFailed(
+                    _('Too many login attempts, user locked out for 15 minutes'),
+                    code='too_many_login_attempts',
+                )
+
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+
+class InvalidLoginAttemptsCache(object):
+    @staticmethod
+    def _key(email):
+        return 'invalid_login_attempt_{}'.format(email)
+
+    @staticmethod
+    def _value(lockout_timestamp, timebucket):
+        return {
+            'lockout_start': lockout_timestamp,
+            'invalid_attempt_timestamps': timebucket
+        }
+
+    @staticmethod
+    def delete(email):
+        try:
+            cache.delete(InvalidLoginAttemptsCache._key(email))
+        except Exception as e:
+            logger.exception(e.message)
+
+    @staticmethod
+    def set(email, timebucket, lockout_timestamp=None):
+        try:
+            key = InvalidLoginAttemptsCache._key(email)
+            value = InvalidLoginAttemptsCache._value(lockout_timestamp, timebucket)
+            cache.set(key, value)
+        except Exception as e:
+            logger.exception(e.message)
+
+    @staticmethod
+    def get(email):
+        try:
+            key = InvalidLoginAttemptsCache._key(email)
+            return cache.get(key)
+        except Exception as e:
+            logger.exception(e.message)
--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
-from pacifica_authentication.views import UserAPIView
+from pacifica_authentication.views import UserAPIView, MyTokenObtainPairView
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -11,7 +11,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     url(r'^', include('pacifica_visualizer.urls')),
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/user/', UserAPIView.as_view(), name='user'),
 ]
